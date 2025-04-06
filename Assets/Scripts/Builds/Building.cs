5d414ea5bafd01b12d66dadb9761e7d75112c48e
Agustin <agustin.exposito@interbrainco.com>
2025-04-05 21:07:23 -0300

    made new ps to show if a structure has energy, made new method to enable drills variables if a stations is placed near,  made new unityevents when enabling energy or storage vars

--- Assets/Scripts/Builds/Building.cs
+++ Assets/Scripts/Builds/Building.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Building : MonoBehaviour
 {
@@ -6,15 +7,62 @@ public class Building : MonoBehaviour
     public bool hasStorage = false;
     public Vector2 searchRadius = new Vector2();
     public ConnectionsManager connectionManager;
+    public UnityEvent onEnergyEnabled;
+    public UnityEvent onEnergyDisabled;
+    public UnityEvent onStorageEnabled;
+    public UnityEvent onStorageDisabled;
 
     public void DefineVariableStates(Collider2D collider)
     {
+        Building currentBuilding = collider.GetComponent<Building>();
         switch (collider.tag)
         {
-            case "EnergyStorage": { hasEnergy = true; hasStorage = true; } break;
-            case "Storage": { hasStorage = true; } break;
-            case "Energy": { hasEnergy = true; } break;
-            default: { Debug.Log("No coincidio con los tags definidos " + collider.tag); } break;
+            case "EnergyStorage": 
+                { 
+                    hasEnergy = true; 
+                    hasStorage = true; 
+                    EnableEnergy(true);
+                    EnableStorage(true);
+                } break;
+            case "Energy": 
+                { 
+                    hasEnergy = true; 
+                    EnableEnergy(true); 
+                } break;
+            case "Storage": 
+                { 
+                    hasStorage = true;
+                    EnableStorage(true); 
+                } break;
         }
+        switch (gameObject.tag)
+        {
+            case "EnergyStorage": 
+                { 
+                    currentBuilding.hasEnergy = true;
+                    currentBuilding.hasStorage = true;
+                    currentBuilding.EnableEnergy(true);
+                    currentBuilding.EnableStorage(true);
+                } break;
+            case "Energy": 
+                { 
+                    currentBuilding.hasEnergy = true; 
+                    currentBuilding.EnableEnergy(true); 
+                } break;
+            case "Storage": 
+                { 
+                    currentBuilding.hasStorage = true; 
+                    currentBuilding.EnableStorage(true); 
+                } break;
+        }
+    }
+
+    public void EnableEnergy(bool state) {
+        if (state) { onEnergyEnabled.Invoke(); }
+        else { onEnergyDisabled.Invoke(); }
+    }
+    public void EnableStorage(bool state) {
+        if (state) { onStorageEnabled.Invoke(); }
+        else { onStorageDisabled.Invoke(); }
     }
 }
